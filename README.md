32ba129030c77447a5d52ed97fb6f16db89d1230
Mr.B <bi.jiankun@cgtn.com>
2017-07-12 20:16:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DataTable React Wrapper
---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Nokey 
+ * @Date: 2017-07-12 15:02:09 
+ * @Last Modified by: Nokey
+ * @Last Modified time: 2017-07-12 15:02:54
+ */
+'use strict'; 
+
+import DataTable from './lib/DataTable';
+
+export default DataTable;
